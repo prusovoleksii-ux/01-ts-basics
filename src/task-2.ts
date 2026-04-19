446194a2a448daf422617df697574f09fd6a103a
prusovoleksii-ux <prusov.oleksii@gmail.com>
2026-04-19 23:31:47 +0200

    initial commit

---
+++ src/task-2.ts
@@ -0,0 +1,13 @@
+interface product {
+    readonly id: number;
+    title?: string;
+    description: string;
+}
+
+const product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
+
+console.log(`Product: ${JSON.stringify(product)}`);
